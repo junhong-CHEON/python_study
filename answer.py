876b0e69fbbc4908560dd8a16ee97af1e1bda863
junhong-CHEON <48478358+junhong-CHEON@users.noreply.github.com>
2021-04-26 16:26:42 +0900

    '재시험답'

--- answer.py
+++ answer.py
@@ -1,18 +1,5 @@
 #%%
-import datetime as dt
-
-while True:
-    myage = int(input("나이를 입력하세요. :"))
-    age = 23
-    
-    if age == myage:
-        print("정답")
-        break
-    else:
-        print("다시")
-
-nowyear = dt.datetime.now().year
-
-print("너는 %d 년생 이구나" %(nowyear - age + 1))
-
+i = 5
+while i < 5:
+    print("*" * i+1)
 # %%
